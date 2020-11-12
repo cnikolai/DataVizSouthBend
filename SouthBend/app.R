0d71b1ba525124e0929a912357df6587fbe139a8
Cynthia Nikolai <cynthianikolai@gmail.com>
2020-11-11 21:41:30 -0500

    Cindy's Panel

--- SouthBend/app.R
+++ SouthBend/app.R
@@ -2,14 +2,27 @@ library(shiny)
 #install.packages("shinythemes")
 library(shinythemes)
 library(sf)
+library(leaflet)
+library(tidyverse)
 
 #global variables
+#setwd("/Users/Cindy/Documents/ND\ MS\ Data\ Science/Data\ Viz/DataVizSouthBend/SouthBend") #for console only
 #setwd("SouthBend") #for console only
-abandoned.properties <- st_read("Abandoned_Property_Parcels.shp")
+abandoned.properties <- st_read("Abandoned_Property_Parcels.shp", stringsAsFactors = FALSE)
 street.lights <- read.csv("Street_Lights.csv")
 parks <- read.csv("Public_Facilities.csv")
 code.enforcement <- read.csv("Code_Enforcement_Cases.csv")
-schools <- st_read("School_Boundaries.shp")
+schools <- st_read("School_Boundaries.shp", stringsAsFactors = FALSE)
+
+#for Cindy's code
+abandoned.properties.no.nas <- abandoned.properties
+code.outcome.names <- unique(abandoned.properties.no.nas$Outcome_St)
+code.outcome.names
+schools.no.nas <- schools
+schools.types <- unique(schools.no.nas$SchoolType)
+schools.types
+pal <- colorFactor(topo.colors(5), code.outcome.names)
+school.pal <- colorFactor(topo.colors(2), schools.types)
 
 
 # Define UI for application that draws a histogram
@@ -33,16 +46,28 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                  # Sidebar with a slider input for number of bins 
                  sidebarLayout(
                      sidebarPanel(
-                         sliderInput("bins",
-                                     "Number of bins:",
-                                     min = 1,
-                                     max = 50,
-                                     value = 30)
-                     ),
+                     #checkboxInput(inputId = "code.outcome",
+                      #             label = "Code Outcome 1", value = FALSE, width = NULL)
+                     checkboxGroupInput(inputId = "code.outcome", 
+                                        label = "Select a Code Outcome", 
+                                        choices = code.outcome.names, 
+                                        selected = NULL, 
+                                        inline = FALSE,
+                                        width = NULL, 
+                                        choiceNames = NULL, 
+                                        choiceValues = NULL),
+                     checkboxGroupInput(inputId = "schools.types",
+                                        label = "School",
+                                        choices = c("Private", "publuc"))
+                     ), #end sidebarPanel
+                     # checkboxGroupInput(inputId = "schools.types", label = "Select a School Type", choices = schools.types, selected = NULL,
+                     #                  inline = FALSE, width = NULL, choiceNames = NULL, choiceValues = NULL)
+
+                 
                      
                      # Show a plot of the generated distribution
                      mainPanel(
-                         plotOutput("distPlot")
+                         leafletOutput(outputId = "code.outcome.map")
                      )
                  )
         )
@@ -53,14 +78,14 @@ ui <- fluidPage(theme = shinytheme("flatly"),
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    code.subset <- reactive({
+        return(abandoned.properties[abandoned.properties$Outcome_St %in% input$code.outcome,])
+    })
+    output$code.outcome.map <- renderLeaflet({
+        leaflet()%>%
+            addTiles()%>%
+            addPolygons(data = abandoned.properties, color = ~pal(code.subset()$Outcome_St)) 
+            #addPolygons(data = schools, color = ~school.pal(code.subset()))
     })
 }
 
--- app.R
+++ app.R
@@ -1,10 +1,25 @@
-
 library(shiny)
 library(shinythemes)
 library(leaflet)
 library(sf)
 library(tidyverse)
 
+## GLOBAL VARIABLES/DATA
+abandoned.properties <- st_read("Abandoned_Property_Parcels/Abandoned_Property_Parcels.shp", stringsAsFactors = FALSE)
+street.lights <- read.csv("Street_Lights.csv")
+parks <- read.csv("Public_Facilities.csv")
+code.enforcement <- read.csv("Code_Enforcement_Cases.csv")
+schools <- st_read("School_Boundaries/School_Boundaries.shp", stringsAsFactors = FALSE)
+
+## CINDY'S CODE - START
+abandoned.properties.no.nas <- abandoned.properties
+code.outcome.names <- unique(abandoned.properties.no.nas$Outcome_St)
+code.outcome.names
+schools.no.nas <- schools
+schools.types <- unique(schools.no.nas$SchoolType)
+schools.types
+pal <- colorFactor(topo.colors(5), code.outcome.names)
+school.pal <- colorFactor(topo.colors(2), schools.types)
 
 
 ##AVISEK'S CODE - START
@@ -49,7 +64,7 @@ parkLocDf.spatial$Popup_Text <- paste("Park Name: ", parkLocDf.spatial$Park_Name
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("flatly"), 
                 
-                titlePanel("Project - Data Visualisation"),
+                titlePanel("Project - Data Visualization"),
                 
                 navbarPage(
                     "Header",
@@ -80,20 +95,31 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                              h3("This is the third panel")
                     ),
                     tabPanel("Cindy's Page",
-                             h3("This is the fourth panel"),
                              # Sidebar with a slider input for number of bins 
                              sidebarLayout(
                                  sidebarPanel(
-                                     sliderInput("bins",
-                                                 "Number of bins:",
-                                                 min = 1,
-                                                 max = 50,
-                                                 value = 30)
-                                 ),
+                                     #checkboxInput(inputId = "code.outcome",
+                                     #             label = "Code Outcome 1", value = FALSE, width = NULL)
+                                     checkboxGroupInput(inputId = "code.outcome", 
+                                                        label = "Select a Code Outcome", 
+                                                        choices = code.outcome.names, 
+                                                        selected = NULL, 
+                                                        inline = FALSE,
+                                                        width = NULL, 
+                                                        choiceNames = NULL, 
+                                                        choiceValues = NULL),
+                                     checkboxGroupInput(inputId = "schools.types",
+                                                        label = "School",
+                                                        choices = c("Private", "publuc"))
+                                 ), #end sidebarPanel
+                                 # checkboxGroupInput(inputId = "schools.types", label = "Select a School Type", choices = schools.types, selected = NULL,
+                                 #                  inline = FALSE, width = NULL, choiceNames = NULL, choiceValues = NULL)
+                                 
+                                 
                                  
                                  # Show a plot of the generated distribution
                                  mainPanel(
-                                     plotOutput("distPlot")
+                                     leafletOutput(outputId = "code.outcome.map")
                                  )
                              )
                     )
@@ -104,7 +130,18 @@ ui <- fluidPage(theme = shinytheme("flatly"),
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    #CINDY'S CODE
+    code.subset <- reactive({
+        return(abandoned.properties[abandoned.properties$Outcome_St %in% input$code.outcome,])
+    })
+    output$code.outcome.map <- renderLeaflet({
+        leaflet()%>%
+            addTiles()%>%
+            addPolygons(data = abandoned.properties, color = ~pal(code.subset()$Outcome_St)) 
+        #addPolygons(data = schools, color = ~school.pal(code.subset()))
+    })
     
+    #AVISEK'S CODE
     output$leafLetPlot <- renderLeaflet({
         
         if(input$park){
